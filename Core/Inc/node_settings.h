b154fc470651e581348b9f3d64a39e854f6b405c
Jeffrey Zhen <jlrzhen@uwaterloo.ca>
2024-11-28 13:01:54 -0500

    add configuration notes

--- Core/Inc/node_settings.h
+++ Core/Inc/node_settings.h
@@ -9,6 +9,15 @@
 #define INC_NODE_SETTINGS_H_
 
 #define NODE_ID 1 // 0 if anonymous, otherwise between 1 and 127
-#define ESC_INDEX 0 // between 0 and 32
+
+/* Determines which ESC is controlled by flight controller.
+ *
+ * E.g. for a 4-rotor setup, we should set ESC_INDEX to 0, 1, 2, 3 for each
+ * CAN ESC adapter board respectively.
+ * 
+ * ESC_INDEX is currently set to 0, this means the current configuration
+ * is for the 1st CAN ESC adapter board attached to the flight controller.
+ */
+define ESC_INDEX 0 // between 0 and 32
 
 #endif /* INC_NODE_SETTINGS_H_ */
